7fa8dde508a5fde112c6b25bd0a496c5916445cf
apoorvpandey0 <apoorvpandey0@gmail.com>
2021-06-18 11:55:33 +0530

    Added firebase based storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'dart:io';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:video_player/video_player.dart';
 
-void main() => runApp(MyApp());
+import 'package:firebase_storage/firebase_storage.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -25,7 +32,24 @@ class _MyHomePageState extends State<MyHomePage> {
   File? _file;
   final picker = ImagePicker();
   bool _isVideo = false;
+  bool isUploading = false;
   VideoPlayerController? _controller;
+  FirebaseStorage storage = FirebaseStorage.instance;
+  List<String> imageUrls = [];
+  List<String> videoUrls = [];
+
+  Future uploadImageToFirebase(
+      BuildContext context, File _file, String folder) async {
+    Reference ref =
+        storage.ref().child("${folder}/file_" + DateTime.now().toString());
+    UploadTask uploadTask = ref.putFile(_file);
+    print("Starting upload");
+    final res = await uploadTask.then((res) {
+      res.ref.getDownloadURL().then((value) => {print(value)});
+    });
+    print(res);
+    print("Finished upload");
+  }
 
   Future getImage(ImageSource imageSource) async {
     var pickedFile = await picker.getImage(source: imageSource);
@@ -76,19 +100,54 @@ class _MyHomePageState extends State<MyHomePage> {
     _controller!.play();
   }
 
+  Future getItems() async {
+    imageUrls.clear();
+    videoUrls.clear();
+    var res = await storage.ref("images").listAll();
+    for (var item in res.items) {
+      String url = await item.getDownloadURL();
+      imageUrls.add(url);
+    }
+    res = await storage.ref("videos").listAll();
+    for (var item in res.items) {
+      String url = await item.getDownloadURL();
+      videoUrls.add(url);
+    }
+    setState(() {});
+    print("Results: ${res.items}");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Image Picker Example'),
       ),
-      body: Center(
-        child: _file == null
-            ? Text('No image selected.')
-            : _isVideo
-                ? VideoPlayer(_controller!)
-                : Image.file(_file!),
-      ),
+      body: PageView(children: [
+        Center(
+          child: _file == null
+              ? Text('No image selected.')
+              : _isVideo
+                  ? VideoPlayer(_controller!)
+                  : Image.file(_file!),
+        ),
+        ListView.builder(
+            itemCount: imageUrls.length + videoUrls.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 300,
+                  // color: Colors.amber,
+                  child: index < imageUrls.length
+                      ? Image.network(imageUrls[index])
+                      : Container(
+                          color: Colors.blue,
+                          child: Text(videoUrls[index - imageUrls.length])),
+                ),
+              );
+            })
+      ]),
       floatingActionButton: Wrap(children: [
         FloatingActionButton(
           tooltip: 'Pick Image',
@@ -147,6 +206,35 @@ class _MyHomePageState extends State<MyHomePage> {
             },
             tooltip: 'Add a video',
             child: Icon(Icons.add)),
+        SizedBox(
+          width: 10,
+        ),
+        FloatingActionButton(
+          onPressed: () async {
+            setState(() {
+              isUploading = true;
+            });
+            await uploadImageToFirebase(
+                context, _file!, _isVideo ? "videos" : "images");
+            setState(() {
+              isUploading = false;
+            });
+          },
+          child: isUploading
+              ? CircularProgressIndicator(
+                  color: Colors.white,
+                )
+              : Icon(Icons.upload),
+        ),
+        SizedBox(
+          width: 10,
+        ),
+        FloatingActionButton(
+          onPressed: () async {
+            await getItems();
+          },
+          child: Icon(Icons.download),
+        )
       ]),
     );
   }
